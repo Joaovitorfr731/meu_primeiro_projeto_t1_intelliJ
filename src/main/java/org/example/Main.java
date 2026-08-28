0aebe7ab116a37084e12e6a58cb067a86fbc94fb
57569557808 <joaovitorfigueiredorosa99@gmail.com>
2026-08-28 16:29:58 -0300

    Atividade 1 Nivel 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,17 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        short valorShort = 100;
-        int valorInt = valorShort;
-        byte valorByte = (byte) valorShort;
+        int numero1 = 10;
+        int numero2 = 20;
 
-        System.out.println("\n4. Diferença entre Tipos de Dados:");
-        System.out.println("Valor short: " + valorShort);
-        System.out.println("Valor int: " + valorInt);
-        System.out.println("Valor byte: " + valorByte);
+        System.out.println("1 - Comparação de Dois Números");
+
+        System.out.println("numero1 > numero2: " + (numero1 > numero2));
+        System.out.println("numero1 < numero2: " + (numero1 < numero2));
+        System.out.println("numero1 >= numero2: " + (numero1 >= numero2));
+        System.out.println("numero1 <= numero2: " + (numero1 <= numero2));
+        System.out.println("numero1 == numero2: " + (numero1 == numero2));
+        System.out.println("numero1 != numero2: " + (numero1 != numero2));
 
     }
 }
