679a265d9c380dcca1d7da036b13f5a6d60f1d38
Joaovitorfr731 <joaovitorfigueiredorosa99@gmail.com>
2026-09-01 14:39:05 -0300

    Modify Main.java to swap values of A and B

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,17 @@ public class Main {
     public static void main(String[] argumentos) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.print("Digite o raio do círculo: ");
-        double raio = leitor.nextDouble();
+        System.out.print("Digite o valor de A: ");
+        double valorA = leitor.nextDouble();
+        System.out.print("Digite o valor de B: ");
+        double valorB = leitor.nextDouble();
 
-        double area = Math.PI * raio * raio;
-        System.out.println("A área do círculo é: " + area);
+        System.out.println("Antes da troca: A = " + valorA + ", B = " + valorB);
+
+        double valorTemporario = valorA;
+        valorA = valorB;
+        valorB = valorTemporario;
+
+        System.out.println("Depois da troca: A = " + valorA + ", B = " + valorB);
     }
 }
