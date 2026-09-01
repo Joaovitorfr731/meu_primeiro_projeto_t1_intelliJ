3c1f3fbf36fa712ffb833f0ddf12de2a4133ee23
Joaovitorfr731 <joaovitorfigueiredorosa99@gmail.com>
2026-09-01 14:42:19 -0300

    Modify Main class to calculate interest and total amount

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,15 +4,17 @@ public class Main {
     public static void main(String[] argumentos) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.print("Digite o primeiro número inteiro: ");
-        int primeiro = leitor.nextInt();
-        System.out.print("Digite o segundo número inteiro: ");
-        int segundo = leitor.nextInt();
+        System.out.print("Digite o capital inicial: ");
+        double capital = leitor.nextDouble();
+        System.out.print("Digite a taxa de juros mensal: ");
+        double taxa = leitor.nextDouble();
+        System.out.print("Digite o tempo em meses: ");
+        double tempo = leitor.nextDouble();
 
-        int quociente = primeiro / segundo;
-        int resto = primeiro % segundo;
+        double juros = capital * (taxa / 100) * tempo;
+        double montante = capital + juros;
 
-        System.out.println("Quociente da divisão inteira: " + quociente);
-        System.out.println("Resto da divisão: " + resto);
+        System.out.println("Os juros são: " + juros);
+        System.out.println("O montante final é: " + montante);
     }
 }
