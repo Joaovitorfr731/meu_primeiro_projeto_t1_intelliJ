d9ed8436a5ee01599f43d0dc434246ef2479d3d0
Joaovitorfr731 <joaovitorfigueiredorosa99@gmail.com>
2026-09-01 14:39:54 -0300

    Replace A and B input with weight and height

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,12 @@ public class Main {
     public static void main(String[] argumentos) {
         Scanner leitor = new Scanner(System.in);
 
-        System.out.print("Digite o valor de A: ");
-        double valorA = leitor.nextDouble();
-        System.out.print("Digite o valor de B: ");
-        double valorB = leitor.nextDouble();
+        System.out.print("Digite o peso em kg: ");
+        double peso = leitor.nextDouble();
+        System.out.print("Digite a altura em metros: ");
+        double altura = leitor.nextDouble();
 
-        System.out.println("Antes da troca: A = " + valorA + ", B = " + valorB);
-
-        double valorTemporario = valorA;
-        valorA = valorB;
-        valorB = valorTemporario;
-
-        System.out.println("Depois da troca: A = " + valorA + ", B = " + valorB);
+        double imc = peso / (altura * altura);
+        System.out.println("O IMC é: " + imc);
     }
 }
